0fea38d07cf9f9a7317c6fc217b4accc6bfbdff0
aishu2005-max <ishwaryaakula042@gmail.com>
2025-07-05 16:48:45 +0530

    Add files via upload

---
+++ WEEK-3 6375955-EFCORE-LAB1/Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using RetailInventory.Models;
+namespace RetailInventory.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+
+    // Navigation property
+    public List<Product>? Products { get; set; }
+}
