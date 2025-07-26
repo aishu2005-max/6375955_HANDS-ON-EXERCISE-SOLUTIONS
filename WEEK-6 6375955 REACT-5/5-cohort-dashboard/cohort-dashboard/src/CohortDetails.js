bf04299a334700ca3e63f232caa55310389f71c6
aishu2005-max <ishwaryaakula042@gmail.com>
2025-07-26 22:12:41 +0530

    Add files via upload

---
+++ WEEK-6 6375955 REACT-5/5-cohort-dashboard/cohort-dashboard/src/CohortDetails.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+function CohortDetails({ cohort }) {
+  const isOngoing = cohort.status.toLowerCase() === 'ongoing';
+  const titleStyle = {
+    color: isOngoing ? 'green' : 'blue',
+  };
+
+  return (
+    <div className={styles.box}>
+      <h3 style={titleStyle}>{cohort.name}</h3>
+      <dl>
+        <dt>Started On</dt>
+        <dd>{cohort.startDate}</dd>
+        <dt>Current Status</dt>
+        <dd>{cohort.status}</dd>
+        <dt>Coach</dt>
+        <dd>{cohort.coach}</dd>
+        <dt>Trainer</dt>
+        <dd>{cohort.trainer}</dd>
+      </dl>
+    </div>
+  );
+}
+
+export default CohortDetails;
