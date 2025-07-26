7d7ea885e0df94c04405280757778a47c2dc6719
aishu2005-max <ishwaryaakula042@gmail.com>
2025-07-26 22:11:18 +0530

    Add files via upload

---
+++ WEEK-6 6375955 REACT-3/3-scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./Components/CalculateScore";
+function App()
+{
+  return(
+    <div>
+      <CalculateScore Name={"Steeve"}
+      School={"DNV Public school"}
+      total={284}
+      goal={3}
+      />
+    </div>
+  )
+}
+export default App;
